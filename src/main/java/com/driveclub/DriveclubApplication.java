92340ab90a39f82d136912cbeeb0287c593f81cf
Mairo022 <mairo022@outlook.com>
2023-10-04 19:40:46 +0200

    Setup project

---
+++ src/main/java/com/driveclub/DriveclubApplication.java
@@ -0,0 +1,13 @@
+package com.driveclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driveclub/DriveclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
