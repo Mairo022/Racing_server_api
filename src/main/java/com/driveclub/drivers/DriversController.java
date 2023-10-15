6926bbf96c515006983386ea39eabf88706587ed
Mairo022 <118812987+Mairo022@users.noreply.github.com>
2023-10-15 16:24:43 +0300

    Correct Driver Pageable parameter parsing

--- src/main/java/com/driveclub/drivers/DriversController.java
+++ src/main/java/com/driveclub/drivers/DriversController.java
@@ -3,6 +3,8 @@ package com.driveclub.drivers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +18,12 @@ public class DriversController {
     @GetMapping(value = "getDrivers")
     public ResponseEntity<Page<DriverDTO>> getDrivers(
             @RequestParam (required = false) String name,
-            @RequestParam Pageable pageable)
+            @PageableDefault (
+                    page = 0,
+                    size = 50,
+                    sort = "money",
+                    direction = Sort.Direction.DESC
+            ) Pageable pageable)
     {
         return ResponseEntity.ok(driversService.getDrivers(pageable, name));
     }
