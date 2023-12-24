1cea98f4ba0f4e45b60097689e14f0d160584d28
Mairo022 <mairo022@outlook.com>
2023-12-24 12:57:03 +0200

    Add request parameter to sessions/getDetails

--- src/main/java/com/driveclub/endpoints/sessions/details/SessionDetailsController.java
+++ src/main/java/com/driveclub/endpoints/sessions/details/SessionDetailsController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -23,7 +24,8 @@ public class SessionDetailsController {
                     page = 0,
                     size = 50
             ) Pageable pageable,
-            UUID sessionID)
+           @RequestParam UUID sessionID
+    )
     {
         return ResponseEntity.ok(sessionDetailsService.getSessionDetails(pageable, sessionID));
     }
